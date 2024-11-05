310189135872e687546fef74d94d2d08004791da
Antoine Bertram <133953320+antoinebertram@users.noreply.github.com>
2024-11-05 20:56:14 +0100

    Added new files to the repository
    Added new index page and pomodoro timer page

---
+++ Scripts/script.js
@@ -0,0 +1,28 @@
+//let numButtonClicks = 0;
+let text = ["Klik nog een keer.",
+            "Heb je er wel echt zin in?",
+            "Stop eens met klikken!",
+            "Als je er echt zoveel zin in had, dan had je wel meer geklikt...",
+            "Wat ben je nou aan het doen?",
+            "Is dit hoe jij je tijd verdrijft?",
+            "YEAHHHHHHH!!!",
+            ":)",
+            "Ik heb die klik niet geregistreed, klik nog een keer.",
+            "Volgens mij gebeurt er nu niks?"];
+let repeated = 0;
+function buttonClicked() {
+    let x = randomInt(0,9);
+    if(repeated != x){
+        document.getElementById("mainDiv").textContent = text[x];
+    }
+    else{
+        buttonClicked();
+        console.log("random int was the same as the previous int");
+    }
+    repeated = x;
+}
+
+function randomInt(min,max){
+    return Math.floor(Math.random()*(max-min)) + min;
+}
+
